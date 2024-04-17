2675c30686ca3517a2303078bc769dc935016ad3
jonluca7 <bongatangjl@gmail.com>
2024-04-17 20:58:55 +0200

    Ajout site gestion connexion / déconnexion

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,7 +3,7 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "La Galerie", "/pages/galerie.html"),
-    new Route("/signin", "Connexion", "/pages/auth/signin.html"),
+    new Route("/signin", "Connexion", "/pages/auth/signin.html" , "/js/auth/signin.js"),
     new Route("/signup", "Inscription", "/pages/auth/signup.html","/js/auth/signup.js"),
     new Route("/account", "Mon Compte", "/pages/auth/account.html"),
     new Route("/editPassword", "Changememt de mot de passe", "/pages/auth/editpassword.html"),
---
+++ js/auth/signin.js
@@ -0,0 +1,28 @@
+const inputMail = document.getElementById("EmailInput");
+const inputPassword = document.getElementById("PasswordInput");
+const btnSignin = document.getElementById("btnSignin");
+
+btnSignin.addEventListener("click" , checkCredentials);
+
+function checkCredentials(){
+
+    // Ici il faudra appeller l'API pour vérifier les crédentials en BDD
+
+     if(inputMail.value == "test@gmail.com" && inputPassword.value == "12345"){
+     alert("Vous êtes connecté");
+
+    //  Il faudra recuperer le vrai Token
+     
+    const token = "bbeberrsrssoeghdpslmqqteyeiiqterte";
+    setToken(token);
+
+    // Placer ce token en Cookie
+
+
+     window.location.replace("/");
+    }
+     else{
+        inputMail.classList.add('is-invalid');
+        inputPassword.classList.add("is-invalid");
+    }
+}
